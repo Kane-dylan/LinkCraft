619e93c2d0441b2f1193bded07157ebe9568ead1
Kane Dylan <cool.kdtech1234@gmail.com>
2025-02-16 23:52:30 +0530

    react update 16/02/25

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-2xl">Shorten URL</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
